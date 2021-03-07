8308da0b15403f001efe51dff50bde348c03cf97
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-07 18:49:58 +0000

    Finished About page

--- PersonalWebsite/PersonalWebsite/Client/Shared/MainLayout.razor.cs
+++ PersonalWebsite/PersonalWebsite/Client/Shared/MainLayout.razor.cs
@@ -19,15 +19,18 @@ namespace PersonalWebsite.Client.Shared
                     BackgroundGrey = "#27272f",
                     Surface = "#373740",
                     DrawerBackground = "#27272f",
-                    DrawerText = "rgba(255,255,255, 0.50)",
-                    DrawerIcon = "rgba(255,255,255, 0.50)",
+                    DrawerText = "rgba(255, 255, 255, 0.50)",
+                    DrawerIcon = "rgba(255, 255, 255, 0.50)",
                     AppbarBackground = "#27272f",
-                    AppbarText = "rgba(255,255,255, 0.70)",
-                    TextPrimary = "rgba(255,255,255, 0.70)",
-                    TextSecondary = "rgba(255,255,255, 0.50)",
+                    AppbarText = "rgba(255, 255, 255, 0.70)",
+                    TextPrimary = "rgba(255, 255, 255, 0.70)",
+                    TextSecondary = "rgba(255, 255, 255, 0.50)",
                     ActionDefault = "#adadb1",
-                    ActionDisabled = "rgba(255,255,255, 0.26)",
-                    ActionDisabledBackground = "rgba(255,255,255, 0.12)"
+                    ActionDisabled = "rgba(255, 255, 255, 0.26)",
+                    ActionDisabledBackground = "rgba(255, 255, 255, 0.12)",
+                    Primary = "#5996e2",
+                    Secondary = "#8ac8a5",
+                    Tertiary = "#ff9381"
                 }
             };
         }
