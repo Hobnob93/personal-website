f3cf37398dee9e516f9f974f77f376d137718b0e
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-14 13:48:29 +0000

    Implemented automata board toolbar

---
+++ PersonalWebsite/PersonalWebsite/Shared/Enums/GridSize.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace PersonalWebsite.Shared.Enums
+{
+    public enum GridSize
+    {
+        [Description("XX Small")]
+        ExtraExtraSmall,
+        [Description("X Small")]
+        ExtraSmall,
+        [Description("Small")]
+        Small,
+        [Description("Medium")]
+        Medium,
+        [Description("Large")]
+        Large,
+        [Description("X Large")]
+        ExtraLarge,
+        [Description("XX Large")]
+        ExtraExtraLarge,
+    }
+}
