3548f9ef1324c2c774caa7d5fe0887d0bacc0a80
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-14 14:06:37 +0000

    Dynamically select grid size from toolbar

--- PersonalWebsite/PersonalWebsite/Client/Shared/BoardWidget.razor.cs
+++ PersonalWebsite/PersonalWebsite/Client/Shared/BoardWidget.razor.cs
@@ -8,22 +8,49 @@ namespace PersonalWebsite.Client.Shared
 {
     public partial class BoardWidget
     {
+        private GridSize size;
+
         [Inject]
         public IBoardFactory BoardFactory { get; set; }
 
         [Parameter]
         public BoardType Type { get; set; }
         [Parameter]
+        public GridSize Size { get => size; set => OnGridSizeChanged(value); }
+        
         public int Height { get; set; }
-        [Parameter]
         public int Width { get; set; }
-
         public Board Board { get; set; }
 
 
         protected override void OnInitialized()
         {
+            InitialiseBoard();
+        }
+
+        protected void InitialiseBoard()
+        {
+            (Height, Width) = Size switch
+            {
+                GridSize.ExtraExtraSmall => (11, 40),
+                GridSize.ExtraSmall => (15, 50),
+                GridSize.Small => (20, 60),
+                GridSize.Medium => (25, 70),
+                GridSize.Large => (30, 80),
+                GridSize.ExtraLarge => (35, 90),
+                GridSize.ExtraExtraLarge => (40, 100)
+            };
+
             Board = BoardFactory.BuildBoard(Type, Height, Width);
+        }
+
+        protected void OnGridSizeChanged(GridSize size)
+        {
+            if (this.size == size)
+                return;
+
+            this.size = size;
+            InitialiseBoard();
         }
 
         public void ClearBoard()
