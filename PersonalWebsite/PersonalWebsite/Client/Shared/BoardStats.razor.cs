5aa7b872ea092103cf87cb63dd7d41dd7eb9eca1
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2021-05-16 18:42:44 +0100

    Refactored statistics into component

---
+++ PersonalWebsite/PersonalWebsite/Client/Shared/BoardStats.razor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using PersonalWebsite.Client.Store;
+using PersonalWebsite.Shared.Enums;
+using PersonalWebsite.Shared.Interfaces;
+using PersonalWebsite.Shared.Models;
+
+namespace PersonalWebsite.Client.Shared
+{
+    public partial class BoardStats
+    {
+        [Inject] public IBoardService BoardService { get; set; }
+        [Inject] public IState<BoardState> BoardState { get; set; }
+        [Inject] public HttpClient Http { get; set; }
+        
+        [Parameter] public BoardType BoardType { get; set; }
+
+        public BoardStatItem[] StatItems { get; set; } = Array.Empty<BoardStatItem>();
+        
+        
+        protected override void OnInitialized()
+        {
+            BoardState.StateChanged += BoardStateChanged;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                StatItems = await Http.GetFromJsonAsync<BoardStatItem[]>($"api/StatisticItems?type={(int)BoardType}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                StatItems = Array.Empty<BoardStatItem>();
+            }
+        }
+
+        private void BoardStateChanged(object? obj, BoardState state)
+        {
+            StateHasChanged();
+        }
+        
+        public new void Dispose()
+        {
+            base.Dispose();
+            BoardState.StateChanged -= BoardStateChanged;
+        }
+    }
+}
---
+++ PersonalWebsite/PersonalWebsite/Shared/Interfaces/IBoardStatItemFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using PersonalWebsite.Shared.Enums;
+using PersonalWebsite.Shared.Models;
+
+namespace PersonalWebsite.Shared.Interfaces
+{
+    public interface IBoardStatItemFactory
+    {
+        IEnumerable<BoardStatItem> GetStatItemsForBoardType(BoardType type);
+    }
+}
